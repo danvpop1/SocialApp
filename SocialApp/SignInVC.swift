f21bcc7705f5f382c33fb4851e91d9d64052f76c
Bogdan Razvan <br.razvan@gmail.com>
2017-06-17 15:42:25 +0300

    firebase setup
    firebase setup

---
+++ SocialApp/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by bogdan razvan on 17/06/2017.
+//  Copyright © 2017 bogdan razvan. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
